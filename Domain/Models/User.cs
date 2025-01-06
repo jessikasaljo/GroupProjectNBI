2a7313487b1963d8c4587245c8d523128ffcb1a1
Hanane kh <143598467+Hanane8@users.noreply.github.com>
2025-01-06 22:15:50 +0100

    Resolved merge conflicts

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -11,5 +11,6 @@ namespace Domain.Models
         public string? Address { get; set; }
         public string? Role { get; set; }
         public ICollection<Order>? Orders { get; set; }
+        //
     }
 }
