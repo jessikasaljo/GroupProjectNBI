36da8e8635ab5b6a7e950d36deb36b3a62a6073f
MOHAMED <mohamed.ongao@gmail.com>
2025-01-07 10:16:33 +0100

    add cart class and CartItem class

---
+++ Domain/Models/Cart.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace Domain.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+
+        // Foreign key for User
+        [Required]
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        // List of products in the cart with quantity
+        public List<CartItem> Items { get; set; } = new();
+
+    }
+}
---
+++ Domain/Models/CartItem.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace Domain.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        // Foreign key for Product
+        [Required]
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+
+
+        // Quantity of the product in the cart
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1.")]
+        public int Quantity { get; set; }
+    }
+}
