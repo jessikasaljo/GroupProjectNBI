4add669455d913bf5ecf5a92827d444a680d9769
jessikasaljo <125219232+jessikasaljo@users.noreply.github.com>
2025-01-14 12:56:10 +0100

    Added seed data

--- Infrastructure/Database/RealDatabase.cs
+++ Infrastructure/Database/RealDatabase.cs
@@ -15,7 +15,7 @@ namespace Infrastructure.Database
         public DbSet<StoreItem> StoreItems { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
-        
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Product>()
